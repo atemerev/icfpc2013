59899a4e35d173bbcc78e73725a83176d0757f45
Ivan Tarasov <Ivan.Tarasov@gmail.com>
2013-08-08 20:36:03 -0700

    Add a very simple HTTP server

---
+++ src/unmatched-fake-server/Unmatched/FakeServer.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Blaze.ByteString.Builder (copyByteString)
+--import qualified Data.ByteString.UTF8 as BU
+import Data.Monoid
+import Network.HTTP.Types (status200, status404)
+import Network.Wai (Application, Response(..), pathInfo)
+import Network.Wai.Handler.Warp (run)
+import System.IO (putStrLn)
+
+main = do
+  let port = 8888
+  putStrLn $ "FakeServer is listening on port " ++ show port
+  run port fakeServer
+
+fakeServer :: Application
+fakeServer req = return $
+  case pathInfo req of
+    ["eval"] -> eval
+    _ -> notFound
+
+notFound = ResponseBuilder status404 [("Content-Type", "text/plain")] $ mconcat $ map copyByteString ["not found"]
+eval = ResponseBuilder status200 [("Content-Type", "text/plain")] $ mconcat $ map copyByteString ["ok"]
+
