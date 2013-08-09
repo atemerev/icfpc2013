ae85cee958c9a93cb299a52c938c8074aff335e4
Dmitry Astapov <dastapov@gmail.com>
2013-08-09 16:29:59 +0300

    First draft of restricted generators

--- src/solver-lib/Gen.hs
+++ src/solver-lib/Gen.hs
@@ -5,26 +5,52 @@ import Test.SmallCheck
 import Test.SmallCheck.Series
 import Control.Applicative
 import Control.Monad
+import qualified Data.Set as S
 
 generateAll :: Int -> [Exp]
 generateAll n = list n serProg
 
-serProg:: Monad m => Series m Exp
-serProg = decDepth serExp -- remove 1 level of depth for top-level lambda
 
-serExp :: (Monad m) => Series m Exp
-serExp = do
+-- Generators are restricted to allowed function set
+data OpName = Not_op | Shl1_op | Shr1_op | Shr4_op | Shr16_op | And_op | Or_op | Xor_op | Plus_op | If_op | Fold_op deriving (Eq, Ord)
+type Restriction = S.Set OpName
+
+allowedUnaryOps :: Restriction -> [Exp -> Exp]
+allowedUnaryOps r = [Not, Shl1, Shr1, Shr4, Shr16] -- TODO
+
+allowedBinaryOps :: Restriction -> [Exp -> Exp -> Exp]
+allowedBinaryOps r = [And, Or, Xor, Plus] -- TODO
+
+allowedIf :: Restriction -> Bool
+allowedIf r = If_op `S.member` r
+  
+allowedFold :: Restriction -> Bool
+allowedFold r = Fold_op `S.member` r
+
+generateRestricted :: Bool -> Int -> Restriction -> [Exp]
+generateRestricted tfold n restriction = 
+  if tfold
+  then map (\e -> Fold MainArg Zero e) $ list (n-4) foldBodies -- |fold x 0| is 2 + 1 + 1, hence n-4
+  else list n serProg
+  where
+    foldBodies = do
+      n <- getDepth
+      (e, hasFold) <- serExp' n (S.delete Fold_op restriction) InFoldBody -- Fold should not be there, but remove it just in case
+      return e
+      
+serProg:: Monad m => Series m Exp
+serProg = decDepth serExpression -- remove 1 level of depth for top-level lambda
+
+serExpression :: (Monad m) => Series m Exp
+serExpression = do
   n <- getDepth
-  (e, hasFold) <- serExp' n NoFold
+  (e, hasFold) <- serExp' n (S.fromList [Not_op, Shl1_op, Shr1_op, Shr4_op, Shr16_op, And_op, Or_op, Xor_op, Plus_op, If_op, Fold_op]) NoFold -- TODO S.fromList
   return e
 
 data FoldState = NoFold -- Allowed to generate unrestricted expression except references to fold args
                | ExternalFold -- Not allowed to generate folds or references to fold args
                | InFoldBody -- Allowed to generate references to fold args but not folds
                deriving (Eq, Show)
-
--- Names of the operations that we are allowed to use while generating
-type Restriction = [Exp]
 
 oneof :: (Monad m) => [Series m a] -> Series m a
 oneof xs = foldr mplus mzero xs
@@ -33,48 +59,44 @@ elements :: (Monad m) => [a] -> Series m a
 elements = oneof . map return
 
 -- Generates (expression, does it contain a fold?)
-serExp' :: (Monad m) => Int -> FoldState -> Series m (Exp, Bool)
-serExp' 1 InFoldBody = oneof $ map (\x -> return (x, False)) [Zero, One, MainArg, Fold1Arg, Fold2Arg]
-serExp' 1 _ = oneof $ map (\x -> return (x, False)) [Zero, One, MainArg]
-serExp' n fs = oneof $ concat [
-  if n >= 4 then [serIf n fs] else [],
-  if (n >= 5 && fs == NoFold) then [serFold n] else [],
-  if n >= 2 then map (\op -> serUnop n fs op) [Not, Shl1, Shr1, Shr4, Shr16] else [], 
-  if n >= 3 then map (\op -> serBinop n fs op) [And, Or, Xor, Plus] else []]
+serExp' :: (Monad m) => Int -> Restriction -> FoldState -> Series m (Exp, Bool)
+serExp' 1 _ InFoldBody = oneof $ map (\x -> return (x, False)) [Zero, One, MainArg, Fold1Arg, Fold2Arg]
+serExp' 1 _ _ = oneof $ map (\x -> return (x, False)) [Zero, One, MainArg]
+serExp' n restriction fs = oneof $ concat [
+  if (n >= 4 && allowedIf restriction) then [serIf n restriction fs] else [],
+  if (n >= 5 && fs == NoFold && allowedFold restriction) then [serFold n (S.delete Fold_op restriction)] else [],
+  if n >= 2 then map (\op -> serUnop n restriction fs op) (allowedUnaryOps restriction) else [], 
+  if n >= 3 then map (\op -> serBinop n restriction fs op) (allowedBinaryOps restriction) else []]
 
-serIf :: (Monad m) => Int -> FoldState -> Series m (Exp, Bool)
-serIf n fs = do
+serIf :: (Monad m) => Int -> Restriction -> FoldState -> Series m (Exp, Bool)
+serIf n restriction fs = do
   sizeA <- elements [1..n - 3]
   sizeB <- elements [1..n - 2 - sizeA]
   let sizeC = n - 1 - sizeA - sizeB
-  (a, foldA) <- serExp' sizeA fs
-  (b, foldB) <- serExp' sizeB (if foldA then ExternalFold else fs)
-  (c, foldC) <- serExp' sizeC (if (foldA || foldB) then ExternalFold else fs)
+  (a, foldA) <- serExp' sizeA restriction fs
+  (b, foldB) <- serExp' sizeB restriction (if foldA then ExternalFold else fs)
+  (c, foldC) <- serExp' sizeC restriction (if (foldA || foldB) then ExternalFold else fs)
   return (If a b c, foldA || foldB || foldC)
 
-serFold :: (Monad m) => Int -> Series m (Exp, Bool)
-serFold n = do
+serFold :: (Monad m) => Int -> Restriction -> Series m (Exp, Bool)
+serFold n restriction = do
   sizeArg <- elements [1..n - 4]
   sizeSeed <- elements [1..n - 3 - sizeArg]
   let sizeBody = n - 2 - sizeArg - sizeSeed
-  (a, foldA) <- serExp' sizeArg ExternalFold
-  (b, foldB) <- serExp' sizeSeed ExternalFold
-  (c, foldC) <- serExp' sizeBody InFoldBody
+  (a, foldA) <- serExp' sizeArg restriction ExternalFold
+  (b, foldB) <- serExp' sizeSeed restriction ExternalFold
+  (c, foldC) <- serExp' sizeBody restriction InFoldBody
   return (Fold a b c, True)
 
-serUnop :: (Monad m) => Int -> FoldState -> (Exp -> Exp) -> Series m (Exp, Bool)
-serUnop n fs op = do
-  (a, foldA) <- serExp' (n-1) fs
+serUnop :: (Monad m) => Int -> Restriction -> FoldState -> (Exp -> Exp) -> Series m (Exp, Bool)
+serUnop n restriction fs op = do
+  (a, foldA) <- serExp' (n-1) restriction fs
   return (op a, foldA)
 
-serBinop :: (Monad m) => Int -> FoldState -> (Exp -> Exp -> Exp) -> Series m (Exp, Bool)
-serBinop n fs op = do
+serBinop :: (Monad m) => Int -> Restriction -> FoldState -> (Exp -> Exp -> Exp) -> Series m (Exp, Bool)
+serBinop n restriction fs op = do
   sizeA <- elements [1..n - 2]
   let sizeB = n - 1 - sizeA
-  (a, foldA) <- serExp' sizeA fs
-  (b, foldB) <- serExp' sizeB (if foldA then ExternalFold else fs)
+  (a, foldA) <- serExp' sizeA restriction fs
+  (b, foldB) <- serExp' sizeB restriction (if foldA then ExternalFold else fs)
   return (op a b, foldA || foldB)
-
--- names of the ops for limiting sets of possible ops
-
--- data RestrictedExp = RExp { exp::Exp, foldState::FoldState, ops::[OpName] }
