b34dbd48eed04d5d81696dcc71a1bfb5396203a3
Dmitry Astapov <dastapov@gmail.com>
2013-08-09 09:48:58 +0300

    URLs for talking to server

---
+++ src/server-api/URLs.hs
@@ -0,0 +1,12 @@
+module URLs where
+
+mkURL path = "http://icfpc2013.cloudapp.net/" ++ path ++ "?auth=" ++ authToken
+ where
+   authToken = teamId ++ "vpsH1H"
+   teamId = "0073xfoSG1mtZmZIyt70v4IPf1SnbJfcBVgkjRjj" 
+
+myproblems = mkURL "myproblems"
+train = mkURL "train"
+eval = mkURL "eval"
+guess = mkURL "guess"
+
