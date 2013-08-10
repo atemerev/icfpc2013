70dacbbe55246cb44aaa8a2f0813add2a605f17b
Eugene Kirpichov <ekirpichov@gmail.com>
2013-08-09 21:52:49 -0700

    benchmark with eval

--- src/test/benchmark.hs
+++ src/test/benchmark.hs
@@ -5,6 +5,6 @@ import Gen
 
 stat ps = stat' 0 0 ps
 stat' !n !s [] = (n, s)
-stat' !n !s (p:ps) = stat' (n+1) (s + 1 {-eval 0 0 0 p-} ) ps
+stat' !n !s (p:ps) = stat' (n+1) (s + eval 0 0 0 p) ps
 
-main = print $ length $ list 10 (serProg noRestriction)
+main = print $ stat $ list 11 (serProg noRestriction)
