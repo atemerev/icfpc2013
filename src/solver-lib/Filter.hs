d6101fb1342c088074ca5dc0cc8b09b72f07d460
Roman Cheplyaka <roma@ro-che.info>
2013-08-10 09:23:56 +0300

    Revert "Unboxed eval - supposed to be faster"
    This reverts commit 6d7f606195f86689a5a643d770bfc7aec731a739.
    
    It breaks the evaluator. (Please run the tests when you change something.)
    
    The reason is that it casts W64 to W32.
    
    Feel free to fix it by using W64# from GHC.Word and recommit.

--- src/solver-lib/Filter.hs
+++ src/solver-lib/Filter.hs
@@ -10,5 +10,5 @@ filterProgs
 filterProgs ins outs =
   filter $ \prog -> and $
     zipWith
-      (\inp outp -> eval inp 0 0 prog == outp)
+      (\inp outp -> eval inp undefined undefined prog == outp)
       ins outs
