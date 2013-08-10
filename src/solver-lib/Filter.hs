6d7f606195f86689a5a643d770bfc7aec731a739
Eugene Kirpichov <ekirpichov@gmail.com>
2013-08-09 21:55:48 -0700

    Unboxed eval - supposed to be faster

--- src/solver-lib/Filter.hs
+++ src/solver-lib/Filter.hs
@@ -10,5 +10,5 @@ filterProgs
 filterProgs ins outs =
   filter $ \prog -> and $
     zipWith
-      (\inp outp -> eval inp undefined undefined prog == outp)
+      (\inp outp -> eval inp 0 0 prog == outp)
       ins outs
--- src/solver-lib/Types.hs
+++ src/solver-lib/Types.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, DeriveDataTypeable, BangPatterns #-}
+{-# LANGUAGE GADTs, DeriveDataTypeable, BangPatterns, MagicHash #-}
 module Types
   ( Exp(..)
   , Word64
@@ -9,6 +9,8 @@ module Types
   )
   where
 
+import GHC.Prim
+import GHC.Types
 import Data.Word
 import Data.Bits
 import Data.Data (Data)
@@ -51,38 +53,48 @@ isConstExpr (Xor a b) = isConstExpr a && isConstExpr b
 isConstExpr (Plus a b) = isConstExpr a && isConstExpr b
 
 eval :: Word64 -> Word64 -> Word64 -> Exp -> Word64
-eval main fold1 fold2 e = case e of
-    Zero -> 0
-    One -> 1
+eval main fold1 fold2 e = case (fromIntegral main, fromIntegral fold1, fromIntegral fold2) of
+  (W# main#, W# fold1#, W# fold2#) -> fromIntegral $ W# (eval' main# fold1# fold2# e)
+
+eval' :: Word# -> Word# -> Word# -> Exp -> Word#
+eval' main fold1 fold2 e = case e of
+    Zero -> 0##
+    One -> 1##
     MainArg -> main
     Fold1Arg -> fold1
     Fold2Arg -> fold2
-    If c t f -> if eval main fold1 fold2 c == 0
-                then eval main fold1 fold2 t
-                else eval main fold1 fold2 f
-    Not e -> complement (eval main fold1 fold2 e)
-    Shl1 e -> shiftL (eval main fold1 fold2 e) 1
-    Shr1 e -> shiftR (eval main fold1 fold2 e) 1
-    Shr4 e -> shiftR (eval main fold1 fold2 e) 4
-    Shr16 e -> shiftR (eval main fold1 fold2 e) 16
-    And e1 e2 -> eval main fold1 fold2 e1 .&. eval main fold1 fold2 e2
-    Or e1 e2 -> eval main fold1 fold2 e1 .|. eval main fold1 fold2 e2
-    Xor e1 e2 -> xor (eval main fold1 fold2 e1) (eval main fold1 fold2 e2)
-    Plus e1 e2 -> eval main fold1 fold2 e1 + eval main fold1 fold2 e2
-    Fold arg seed body -> foldImpl (eval main fold1 fold2 arg) (eval main fold1 fold2 seed) body
-  where foldImpl !x !seed body = op x0 (op x1 (op x2 (op x3 (op x4 (op x5 (op x6 (op x7 seed)))))))
+    If c t f -> if word2Int# (eval' main fold1 fold2 c) ==# 0#
+                then eval' main fold1 fold2 t
+                else eval' main fold1 fold2 f
+    Not e -> not# (eval' main fold1 fold2 e)
+    Shl1 e -> uncheckedShiftL# (eval' main fold1 fold2 e) 1#
+    Shr1 e -> uncheckedShiftRL# (eval' main fold1 fold2 e) 1#
+    Shr4 e -> uncheckedShiftRL# (eval' main fold1 fold2 e) 4#
+    Shr16 e -> uncheckedShiftRL# (eval' main fold1 fold2 e) 16#
+    And e1 e2 -> eval' main fold1 fold2 e1 `and#` eval' main fold1 fold2 e2
+    Or e1 e2 -> eval' main fold1 fold2 e1 `or#` eval' main fold1 fold2 e2
+    Xor e1 e2 -> eval' main fold1 fold2 e1 `xor#` eval' main fold1 fold2 e2
+    Plus e1 e2 -> eval' main fold1 fold2 e1 `plusWord#` eval' main fold1 fold2 e2
+    Fold arg seed body -> {-# SCC "foldImpl" #-} foldImpl (eval' main fold1 fold2 arg) (eval' main fold1 fold2 seed) body
+  where foldImpl x seed body = op x0 (op x1 (op x2 (op x3 (op x4 (op x5 (op x6 (op x7 seed)))))))
           where
-            op !a !b = eval main a b body
-            {-# INLINE op #-}
+            op :: Word# -> Word# -> Word#
+            op a b = eval' main a b body
             -- x7 is least significant, x0 most significant
-            (!x7', !x7) = x `divMod` 256
-            (!x6', !x6) = x7' `divMod` 256
-            (!x5', !x5) = x6' `divMod` 256
-            (!x4', !x4) = x5' `divMod` 256
-            (!x3', !x3) = x4' `divMod` 256
-            (!x2', !x2) = x3' `divMod` 256
-            (!x1', !x1) = x2' `divMod` 256
-            (!x0', !x0) = x1' `divMod` 256
+            x7654 = narrow32Word# x
+            x3210 = x `uncheckedShiftRL#` 32#
+            x76 = narrow16Word# x7654
+            x54 = x7654 `uncheckedShiftRL#` 16#
+            x32 = narrow16Word# x3210
+            x10 = x3210 `uncheckedShiftRL#` 16#
+            x7 = narrow8Word# x76
+            x6 = x76 `uncheckedShiftRL#` 8#
+            x5 = narrow8Word# x54
+            x4 = x54 `uncheckedShiftRL#` 8#
+            x3 = narrow8Word# x32
+            x2 = x32 `uncheckedShiftRL#` 8#
+            x1 = narrow8Word# x10
+            x0 = x10 `uncheckedShiftRL#` 8#
 
 progSize :: Exp -> Int
 progSize e = expSize e + 1
