23737c68423d63b1fef0b0cd1276a066b8d32168
Eugene Kirpichov <ekirpichov@gmail.com>
2013-08-09 21:00:07 -0700

    Much faster eval

--- src/solver-lib/Types.hs
+++ src/solver-lib/Types.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, DeriveDataTypeable #-}
+{-# LANGUAGE GADTs, DeriveDataTypeable, BangPatterns #-}
 module Types
   ( Exp(..)
   , Word64
@@ -51,39 +51,38 @@ isConstExpr (Xor a b) = isConstExpr a && isConstExpr b
 isConstExpr (Plus a b) = isConstExpr a && isConstExpr b
 
 eval :: Word64 -> Word64 -> Word64 -> Exp -> Word64
-eval main fold1 fold2 e = eval' e
-  where eval' e = case e of {
-          Zero -> 0
-        ; One -> 1
-        ; MainArg -> main
-        ; Fold1Arg -> fold1
-        ; Fold2Arg -> fold2
-        ; If c t f -> if eval' c == 0
-                      then eval' t
-                      else eval' f
-        ; Not e -> complement (eval' e)
-        ; Shl1 e -> shiftL (eval' e) 1
-        ; Shr1 e -> shiftR (eval' e) 1
-        ; Shr4 e -> shiftR (eval' e) 4
-        ; Shr16 e -> shiftR (eval' e) 16
-        ; And e1 e2 -> eval' e1 .&. eval' e2
-        ; Or e1 e2 -> eval' e1 .|. eval' e2
-        ; Xor e1 e2 -> xor (eval' e1) (eval' e2)
-        ; Plus e1 e2 -> eval' e1 + eval' e2
-        ; Fold arg seed body -> foldImpl (eval' arg) (eval' seed) body
-        }
-        foldImpl x seed body = op x0 (op x1 (op x2 (op x3 (op x4 (op x5 (op x6 (op x7 seed)))))))
+eval main fold1 fold2 e = case e of
+    Zero -> 0
+    One -> 1
+    MainArg -> main
+    Fold1Arg -> fold1
+    Fold2Arg -> fold2
+    If c t f -> if eval main fold1 fold2 c == 0
+                then eval main fold1 fold2 t
+                else eval main fold1 fold2 f
+    Not e -> complement (eval main fold1 fold2 e)
+    Shl1 e -> shiftL (eval main fold1 fold2 e) 1
+    Shr1 e -> shiftR (eval main fold1 fold2 e) 1
+    Shr4 e -> shiftR (eval main fold1 fold2 e) 4
+    Shr16 e -> shiftR (eval main fold1 fold2 e) 16
+    And e1 e2 -> eval main fold1 fold2 e1 .&. eval main fold1 fold2 e2
+    Or e1 e2 -> eval main fold1 fold2 e1 .|. eval main fold1 fold2 e2
+    Xor e1 e2 -> xor (eval main fold1 fold2 e1) (eval main fold1 fold2 e2)
+    Plus e1 e2 -> eval main fold1 fold2 e1 + eval main fold1 fold2 e2
+    Fold arg seed body -> foldImpl (eval main fold1 fold2 arg) (eval main fold1 fold2 seed) body
+  where foldImpl !x !seed body = op x0 (op x1 (op x2 (op x3 (op x4 (op x5 (op x6 (op x7 seed)))))))
           where
-            op a b = eval main a b body
+            op !a !b = eval main a b body
+            {-# INLINE op #-}
             -- x7 is least significant, x0 most significant
-            (x7', x7) = x `divMod` 256
-            (x6', x6) = x7' `divMod` 256
-            (x5', x5) = x6' `divMod` 256
-            (x4', x4) = x5' `divMod` 256
-            (x3', x3) = x4' `divMod` 256
-            (x2', x2) = x3' `divMod` 256
-            (x1', x1) = x2' `divMod` 256
-            (x0', x0) = x1' `divMod` 256
+            (!x7', !x7) = x `divMod` 256
+            (!x6', !x6) = x7' `divMod` 256
+            (!x5', !x5) = x6' `divMod` 256
+            (!x4', !x4) = x5' `divMod` 256
+            (!x3', !x3) = x4' `divMod` 256
+            (!x2', !x2) = x3' `divMod` 256
+            (!x1', !x1) = x2' `divMod` 256
+            (!x0', !x0) = x1' `divMod` 256
 
 progSize :: Exp -> Int
 progSize e = expSize e + 1
